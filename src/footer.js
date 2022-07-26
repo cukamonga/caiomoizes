f8a33c7a45a266e77bf3242aac77422f2e71173d
--global <--global>
2022-07-26 05:07:40 -0300

    novo site

---
+++ src/footer.js
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+        <div class="footer">
+            <span style={{ left: 20 }}>Copyright © 2022 Caio Moizés. Todos os direitos reservados.</span>
+            <span style={{ right: 20 }}>Desenvolvido com Next.js.</span>
+        </div>
+    );
+}
+
+export default Footer;
--- src/header.js
+++ src/header.js
@@ -1,16 +1,21 @@
 function Header() {
     return (
-        <html>
-            <head>
-                <title>Caio Moizés - Programador</title>
-                <link rel="stylesheet" href="../src/style.css" />
-            </head>
-            <body>
-                <div class="header">
-                    <p>Teste de texto.</p>
+        <div class="header">
+            <div class="topo">
+                <div class="info">
+                    <h1>Caio Moizés</h1>
+                    <h2>Programador</h2>
+                    <a href="https://api.whatsapp.com/send?phone=5518997524254" target="_blank"><span><i class="fa-brands fa-whatsapp"></i> (18) 99752-4254</span></a>
                 </div>
-            </body>
-        </html>
+            </div>
+            <div class="menu">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Orçamento</a></li>
+                    <li><a href="#">Contato</a></li>
+                </ul>
+            </div>
+        </div>
     );
 }
 
