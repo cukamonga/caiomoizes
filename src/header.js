a2789d3ab6e2c94a4a1fb99d73f87471a5f6bcb8
--global <--global>
2022-07-26 03:26:24 -0300

    novo site

---
+++ src/header.js
@@ -0,0 +1,15 @@
+function Header() {
+    return (
+        <html>
+            <head>
+                <title>Caio Moizés - Programador</title>
+                <link rel="stylesheet" href="style.css" />
+            </head>
+            <body>
+                <div class="header"></div>
+            </body>
+        </html>
+    );
+}
+
+export default Header;
